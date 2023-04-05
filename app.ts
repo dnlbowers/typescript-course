49d5c1a6306bad20a64a62f2944b2a6de603aeef
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-04-06 01:59:25 +0200

    cover object types and nested object types

--- app.ts
+++ app.ts
@@ -1,17 +1,53 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+// You can explicitly define the object like this
+// const person: {
+//     name: string;
+//     age: number;
+//     } = {
+//         name: 'David',
+//         age: 38
+//     };
 
-    const result = n1 + n2;
+// however its better practice to let TS to infer the type of the object like this 
+// however its better practice to let TS to infer the type of the object like this 
+const person = {
+    name: 'David',
+    age: 38
+};
 
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
+// Above Ts will infer the type of the object as:
+// const person: {
+//     name: string;
+//     age: number;
+//     }
+
+
+
+console.log(person.name);
+// Nested Objects & Types
+// Of course object types can also be created for nested objects.
+
+// Let's say you have this JavaScript object:
+
+const product = {
+  id: 'abc1',
+  price: 12.99,
+  tags: ['great-offer', 'hot-and-new'],
+  details: {
+    title: 'Red Carpet',
+    description: 'A great carpet - almost brand-new!'
+  }
 }
+// This would be the type of such an object:
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
+// {
+//   id: string;
+//   price: number;
+//   tags: string[];
+//   details: {
+//     title: string;
+//     description: string;
+//   }
+// }
+// So you have an object type in an object type so to say.
 
-add(number1, number2, printResult, resultPhrase);
+console.log(product.details.title);
