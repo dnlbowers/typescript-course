7c971ff35f84bbee0c36f2e3dbf920d7ff5f0c81
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-04-11 02:01:27 +0200

    complete lessons on interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,135 +1,76 @@
-abstract class Department {
-    static fiscalYear = 2020;
-    // private name: string;
-    protected employees: string[] = [];
-
-    constructor(protected readonly id: string, public name: string) {
-        // this.name = n;
-    }
-    //methods
-
-    static createEmployee(name: string) {
-        return { name: name };
-    }
-
-    abstract describe(this: Department): void ;
-
-    addEmployee(employee: string) {
-        // this.id = 'd2'
-        this.employees.push(employee);
-    }
-
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-class ITDepartment extends Department {
-
-    admins: string[];
-
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-
-    describe() {
-        console.log('IT Department - ID: ' + this.id)
-    }
-
+// type AddFn = (a: number, b: number) => number;
+// above is the same as below
+interface AddFn {
+    (a: number, b: number): number;
 }
 
-class AccountingDepartment extends Department {
+let add: AddFn;
 
-    private lastReport: string;
-    private static instance: AccountingDepartment;
+add = (n1: number, n2: number) => {
+    return n1 + n2;
+}
 
-    get mostRecentReport() {
-        if (this.lastReport){
-            return this.lastReport;
-        }
-        throw new Error('No report found.');
-    }
+interface Person {
+    /*interfaces set a framework for types cannot take initializer ie. name = max*/
+    name: string;
+    age: number;
 
-    set mostRecentReport(value: string) {
-        if (!value) {
-            throw new Error ("please pass in a valid value")
-        }
-        this.addReport(value)
-    }
+    // just name of method with types but no logic
+    greet(phrase: string): void;
+}
 
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-        this.lastReport = reports[0];
-    }
+let person1: Person;
 
-    static getInstance() {
-        if (AccountingDepartment.instance) {
-            return this.instance;
-        }
-        return this.instance = new AccountingDepartment('d2', [])
-    }
-
-    describe() {
-        console.log('Accounting Department - ID: ' + this.id);
-    }
-
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employees.push(name)
-    }
-
-    addReport(text: string) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    printReports() {
-        console.log(this.reports);
+person1 = {
+    /* valid person1 object because it satisfies the conditions on the above interface */
+    name: 'Max',
+    age: 30,
+    greet(phrase: string) {
+        console.log(phrase + ' ' + this.name);
     }
 }
 
-const employee1 = Department.createEmployee('Max');
+person1.greet('Hi there - I am');
 
-console.log(employee1, Department.fiscalYear)
+interface Named {
+    readonly name?: string;
+    /*readonly makes the property immutable*/
+    outputName?: string; // ? makes the property optional
+}
 
-const it = new ITDepartment('d1', ['David'])
+interface Greetable extends Named {
+    /*interfaces set a framework for types cannot take initializer ie. name = max*/
+    // readonly name: string;
+    // just name of method with types but no logic
+    greet(phrase: string): void;
+}
 
 
+class User implements Greetable {
+    /* can implement several interfaces, implement is a contract that the class has to follow the interface */
+    /*can be used to extend the interface but have to follow the contract of the interface method and types */
+    name?: string;
+    age = 30;
 
-it.addEmployee('Max')
-it.addEmployee('Manu')
+    constructor(n?: string) { //could also make defualt value of name = 'Max' in the constructor
+        if (n){
+            this.name = n;
+        }
+    }
 
-// accounting.employees[2] = 'Anna';// this is not allowed because employees is private
+    greet(phrase: string) {
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        } else {
+            console.log('Hi!');
+        }
+    }
+}
 
-console.log(it)
+let user1: Greetable
+// let user1.name = 'Manu' // cannot reassign name because of readonly in the interface
 
-it.describe();
-it.printEmployeeInformation();
+user1 = new User();
+console.log(user1);
+user1.greet('Hi there - I am');
 
-// const accounting = new AccountingDepartment('d2', []);
-const accounting  = AccountingDepartment.getInstance();
-const accounting2  = AccountingDepartment.getInstance();
-
-console.log(accounting, accounting2);
-
-accounting.mostRecentReport = 'Year End Report';
-// console.log(accounting.mostRecentReport);
-
-accounting.addReport('Something went wrong...');
-console.log("this one " + accounting.mostRecentReport);
-console.log(accounting);
-// accounting.printReports()
-
-accounting.addEmployee('Manu')
-accounting.addEmployee('Max')
-
-// accounting.printEmployeeInformation();
-
-accounting.describe();
-
-// const accountingCopy = {name: 'COE', describe: accounting.describe };
-
-// accountingCopy.describe();
