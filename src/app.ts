f4e644cd4404c355198525bd3c1c009325084abb
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2023-04-18 23:36:37 +0200

    fucntion overloads

--- src/app.ts
+++ src/app.ts
@@ -39,6 +39,8 @@ type Universal = Combinable & Numeric; // this will be a number type because num
 
 // type guards
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
 function add(a: Combinable, b: Combinable) {
     if(typeof a === 'string' || typeof b === 'string') {
         return a.toString() + b.toString();
@@ -46,96 +48,101 @@ function add(a: Combinable, b: Combinable) {
     return a + b;
 }
 
-type UnknownEmployee = Employee | Admin;
+const result = add('Max', 'Schwartz');
+result.split(' ');
 
-function printEmployeeInformation(emp: UnknownEmployee) {
-    console.log('Name ' + emp.name)
-    if('privileges' in emp) {
-        console.log('privileges ' + emp.privileges)
-    }
-    if('startDate' in emp) {
-        console.log('Start date ' + emp.startDate)
-    }
+const result2 = add(1, 2)
+
+// type UnknownEmployee = Employee | Admin;
+
+// function printEmployeeInformation(emp: UnknownEmployee) {
+//     console.log('Name ' + emp.name)
+//     if('privileges' in emp) {
+//         console.log('privileges ' + emp.privileges)
+//     }
+//     if('startDate' in emp) {
+//         console.log('Start date ' + emp.startDate)
+//     }
     
-}
+// }
 
-printEmployeeInformation(e1);
-printEmployeeInformation({name: 'Manu', startDate: new Date()});
+// printEmployeeInformation(e1);
+// printEmployeeInformation({name: 'Manu', startDate: new Date()});
 
-class Car {
-    drive() {
-        console.log('Driving...')
-    }
-}
+// class Car {
+//     drive() {
+//         console.log('Driving...')
+//     }
+// }
 
-class Truck {
-    drive() {
-        console.log('Driving a truck...')
-    }   
+// class Truck {
+//     drive() {
+//         console.log('Driving a truck...')
+//     }   
 
-    loadCargo(amount: number) {
-        console.log('Loading Cargo...' + amount)
-    }
-}
+//     loadCargo(amount: number) {
+//         console.log('Loading Cargo...' + amount)
+//     }
+// }
 
-type Vehicle = Car | Truck;
+// type Vehicle = Car | Truck;
 
-const v1 = new Car();
-const v2 = new Truck();
+// const v1 = new Car();
+// const v2 = new Truck();
 
-function useVehicle(vehicle: Vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000)
-    }
-}
+// function useVehicle(vehicle: Vehicle) {
+//     vehicle.drive();
+//     if (vehicle instanceof Truck) {
+//         vehicle.loadCargo(1000)
+//     }
+// }
 
-useVehicle(v1);
-useVehicle(v2);
+// useVehicle(v1);
+// useVehicle(v2);
 
-interface Bird {
-    type: 'bird';
-    flyingSpeed: number;
-}
+// interface Bird {
+//     type: 'bird';
+//     flyingSpeed: number;
+// }
 
-interface Horse {
-    type: 'horse';
-    runningSpeed: number;
-}
+// interface Horse {
+//     type: 'horse';
+//     runningSpeed: number;
+// }
 
-type Animal = Bird | Horse;
+// type Animal = Bird | Horse;
 
-function moveAnimal(animal: Animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('Moving at speed: ' + speed)
+// function moveAnimal(animal: Animal) {
+//     let speed;
+//     switch (animal.type) {
+//         case 'bird':
+//             speed = animal.flyingSpeed;
+//             break;
+//         case 'horse':
+//             speed = animal.runningSpeed;
+//             break;
+//     }
+//     console.log('Moving at speed: ' + speed)
 
-}
+// }
 
-moveAnimal({type: 'bird', flyingSpeed: 10});
+// moveAnimal({type: 'bird', flyingSpeed: 10});
 
-const paragraph =document.getElementById('message-output');
+// const paragraph =document.getElementById('message-output');
 
-// const userInputElement = <HTMLInputElement>document.getElementById('user-input')! //  type casting but can clash with react syntax
-// const userInputElement = document.getElementById('user-input')  as HTMLInputElement; // type casting 
-const userInputElement = document.getElementById('user-input') 
+// // const userInputElement = <HTMLInputElement>document.getElementById('user-input')! //  type casting but can clash with react syntax
+// // const userInputElement = document.getElementById('user-input')  as HTMLInputElement; // type casting 
+// const userInputElement = document.getElementById('user-input') 
 
-if (userInputElement) {
-    (userInputElement as HTMLInputElement).value = 'Hi there!'
-}
+// if (userInputElement) {
+//     (userInputElement as HTMLInputElement).value = 'Hi there!'
+// }
 
-interface ErrorContainer { // {email: 'Not a valid email', username: 'Must start with a character'}
-    [key: string]: string; // index property
-}
+// interface ErrorContainer { // {email: 'Not a valid email', username: 'Must start with a character'}
+//     [key: string]: string; // index property
+// }
 
-const errorBag: ErrorContainer = {
-    email: 'Not a valid email',
-    username: 'Must start with a capital character'
-};
+// const errorBag: ErrorContainer = {
+//     email: 'Not a valid email',
+//     username: 'Must start with a capital character'
+// };
